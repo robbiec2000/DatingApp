24b4806d538ee05dd6d43a9803102e513292d7f3
Robbie <siboc@student.unimelb.edu.au>
2024-05-10 21:27:03 +1000

    client

--- API/Program.cs
+++ API/Program.cs
@@ -10,12 +10,15 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
-app.UseAuthorization();
+// app.UseHttpsRedirection();
+//
+// app.UseAuthorization();
 
 app.MapControllers();
 
