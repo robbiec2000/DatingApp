04185e06941ccb41b2de3252d82ad52255465d4d
Robbie <siboc@student.unimelb.edu.au>
2024-05-13 20:37:05 +1000

    backend user extend

---
+++ API/Data/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Interface;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class UserRepository : IUserRepository
+{
+    private readonly DataContext _context;
+    private readonly IMapper _mapper;
+
+    public UserRepository(DataContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    
+    public void Update(AppUser user)
+    {
+        _context.Entry(user).State = EntityState.Modified;
+    }
+
+    public async Task<bool> SaveAllAsync()
+    {
+        return await _context.SaveChangesAsync() > 0;
+    }
+
+    public async Task<IEnumerable<AppUser>> GetUsersAsync()
+    {
+        return await _context.Users.Include(user => user.Photos).ToListAsync();
+    }
+
+    public async Task<AppUser> GetUserByIdAsync(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+    public async Task<AppUser> GetUserByUsernameAsync(string username)
+    {
+        return await _context.Users.Include(user => user.Photos).SingleOrDefaultAsync(user => user.Username == username);
+    }
+
+    public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+    {
+        return await _context.Users.ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
+            .ToListAsync();
+    }
+
+    public async Task<MemberDto> GetMemberAsync(string username)
+    {
+        return await _context.Users.Where(x => x.Username == username)
+            .ProjectTo<MemberDto>(_mapper.ConfigurationProvider).SingleOrDefaultAsync();
+    }
+}
