1506d5f5d0229f1f45f419e2a0227fae392caea7
Robbie <siboc@student.unimelb.edu.au>
2024-05-14 23:45:48 +1000

    update profile

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,12 +9,15 @@ import { authGuard } from './_guards/auth.guard';
 import { TestErrorsComponent } from './test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { preventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: '', runGuardsAndResolvers: 'always', canActivate: [authGuard], children: [
     {path: 'members', component: MemberListComponent},
     {path: 'members/:username', component: MemberDetailComponent},
+    {path: 'member/edit', component: MemberEditComponent, canDeactivate: [preventUnsavedChangesGuard]},
     {path: 'lists', component: ListsComponent},
     {path: 'messages', component: MessagesComponent},
   ]},
